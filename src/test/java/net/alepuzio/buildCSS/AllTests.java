38dbab96379fc4078edca062acd5a1642237dafa
Alessandro Puzielli <alessandro.puzielli@alepuzio.net>
2017-10-28 20:26:37 +0200

    Added my SuiteTest for unitary testing

---
+++ src/test/java/net/alepuzio/buildCSS/AllTests.java
@@ -0,0 +1,14 @@
+package net.alepuzio.buildCSS;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ AppTest.class, TestRowCodeCSS.class })
+public class AllTests {
+	/**
+	 * empty
+	 * */
+
+}
